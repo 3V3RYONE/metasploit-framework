3efb772ae7ea1d6caf4019308b88268bb91796c6
3V3RYONE <beleswarprasad@gmail.com>
2022-09-14 10:27:08 +0530

    added spec for HttpTraceHeadersOnly and HttpTraceColors

--- spec/lib/metasploit/framework/login_scanner/http_spec.rb
+++ spec/lib/metasploit/framework/login_scanner/http_spec.rb
@@ -7,37 +7,7 @@ RSpec.describe Metasploit::Framework::LoginScanner::HTTP do
   include_context 'Msf::UIDriver'
   include_context 'Msf::DBManager'
   include_context 'Msf::Simple::Framework'
-
-  # let(:driver) do
-  #   instance = double('Rex::Ui::Text::Output::Stdio', framework: framework)
-  #   #require 'pry';binding.pry
-  #   allow(instance).to receive(:print_line) { |arg| $stdout.puts arg }
-  #   capture_logging(instance)
-  #   instance
-  # end
-
-  #class Metasploit::Framework::LoginScanner::HTTP
-  #  def print_line(mesg='')
-  #    print(mesg+"\n")
-  #  end
-  #end
-
-  #class Rex::Proto::Http::Packet
-  #  def to_terminal_output(headers_only=false)
-  #    output_packet(true, headers_only=headers_only)
-  #  end
-
-  #  def to_s(headers_only=false)
-  #    output_packet(false, headers_only=headers_only)
-  #  end
-  #end
-
-  #class Rex::Ui::Text::Output::Stdio
-  #  def support_color?
-  #    return false
-  #  end
-  #end
-
+  
   it_behaves_like 'Metasploit::Framework::LoginScanner::Base',  has_realm_key: true, has_default_realm: false
   it_behaves_like 'Metasploit::Framework::LoginScanner::RexSocket'
   it_behaves_like 'Metasploit::Framework::LoginScanner::HTTP'
@@ -55,8 +25,6 @@ RSpec.describe Metasploit::Framework::LoginScanner::HTTP do
     allow_any_instance_of(Rex::Proto::Http::Client).to receive(:request_cgi).with(any_args)
     allow_any_instance_of(Rex::Proto::Http::Client).to receive(:send_recv).with(any_args).and_return(response)
     allow_any_instance_of(Rex::Proto::Http::Client).to receive(:set_config).with(any_args)
-    #allow().to receive(:print_line) { |arg| $stdout.puts arg }
-    allow(subject).to receive(:print) { |arg| $stdout.puts arg }
     allow_any_instance_of(Rex::Proto::Http::Client).to receive(:close)
     allow_any_instance_of(Rex::Proto::Http::Client).to receive(:connect)
   end
@@ -74,8 +42,8 @@ RSpec.describe Metasploit::Framework::LoginScanner::HTTP do
       "GET / HTTP/1.1\nHost: www.google.com"
     }
     let(:sample_response) {
-      res = Rex::Proto::Http::Response.new
-      allow(res).to receive(:body).and_return("HTTP/1.1 302 Found\nLocation: https://www.google.com/?gws_rd=ssl")
+      res = Rex::Proto::Http::Response.new(302,'Found')
+      allow(res).to receive(:body).and_return("Location: https://www.google.com/?gws_rd=ssl")
       res
     }
     let(:expected_output) {
@@ -88,9 +56,8 @@ RSpec.describe Metasploit::Framework::LoginScanner::HTTP do
         "####################",
         "# Response:",
         "####################",
-        "%clr%bld%bluHTTP/1.1 200 OK\r",
+        "%clr%bld%bluHTTP/1.1 302 Found\r",
         "\r",
-        "HTTP/1.1 302 Found",
         "Location: https://www.google.com/?gws_rd=ssl%clr"
       ]
     }
@@ -104,9 +71,36 @@ RSpec.describe Metasploit::Framework::LoginScanner::HTTP do
         "####################",
         "# Response:",
         "####################",
-        "%clr%bld%bluHTTP/1.1 200 OK\r",
+        "No response received"
+      ]
+    }
+    let(:headers_only_output) {
+      [
+        "####################",
+        "# Request:",
+        "####################",
+        "%clr%bld%redGET / HTTP/1.1",
+        "Host: www.google.com%clr",
+        "####################",
+        "# Response:",
+        "####################",
+        "%clr%bld%bluHTTP/1.1 302 Found\r",
         "\r",
-        "HTTP/1.1 302 Found",
+        "%clr"
+      ]
+    }
+    let(:http_trace_colors_output) {
+      [
+        "####################",
+        "# Request:",
+        "####################",
+        "%clr%bld%bluGET / HTTP/1.1",
+        "Host: www.google.com%clr",
+        "####################",
+        "# Response:",
+        "####################",
+        "%clr%bld%grnHTTP/1.1 302 Found\r",
+        "\r",
         "Location: https://www.google.com/?gws_rd=ssl%clr"
       ]
     }
@@ -120,9 +114,19 @@ RSpec.describe Metasploit::Framework::LoginScanner::HTTP do
       expect(@output).to eq expected_output
     end
 
-    it 'should give ideal message for nil response' do
-      subject.set_http_trace_proc(true, false, nil).call(sample_request, sample_response)
+    it 'should give "no response received" message for nil response' do
+      subject.set_http_trace_proc(true, false, nil).call(sample_request, nil)
       expect(@output).to eq nil_response_output
+    end
+
+    it 'should log HTTP headers only when HttpTraceHeadersOnly is set' do
+      subject.set_http_trace_proc(true, true, nil).call(sample_request, sample_response)
+      expect(@output).to eq headers_only_output
+    end
+
+    it 'should log the requests and responses in the specified color' do
+      subject.set_http_trace_proc(true, false, 'blu/grn').call(sample_request, sample_response)
+      expect(@output).to eq http_trace_colors_output
     end
   end
 
