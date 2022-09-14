2ed5fc22c3170f7c70a981152d81518e63b6942b
3V3RYONE <beleswarprasad@gmail.com>
2022-09-14 21:45:33 +0530

    added more specs to check nil parameters

--- spec/lib/metasploit/framework/login_scanner/http_spec.rb
+++ spec/lib/metasploit/framework/login_scanner/http_spec.rb
@@ -67,8 +67,7 @@ RSpec.describe Metasploit::Framework::LoginScanner::HTTP do
         "####################",
         "# Request:",
         "####################",
-        "%clr%bld%redGET / HTTP/1.1",
-        "Host: www.google.com%clr",
+        "%clr%bld%red%clr",
         "####################",
         "# Response:",
         "####################",
@@ -108,6 +107,22 @@ RSpec.describe Metasploit::Framework::LoginScanner::HTTP do
       ]
     }
 
+    let(:http_trace_single_color_output) {
+      [
+        "####################",
+        "# Request:",
+        "####################",
+        "%clr%bld%yelGET / HTTP/1.1",
+        "Host: www.google.com%clr",
+        "####################",
+        "# Response:",
+        "####################",
+        "%clrHTTP/1.1 302 Found\r",
+        "\r",
+        "Location: https://www.google.com/?gws_rd=ssl%clr"
+      ]
+    }
+
     it 'returns a proc object when HttpTrace is set to true' do
       expect(subject.set_http_trace_proc(true, false, nil)).to be_kind_of(Proc)
     end
@@ -115,10 +130,10 @@ RSpec.describe Metasploit::Framework::LoginScanner::HTTP do
     it 'should execute the proc when defined' do
       subject.set_http_trace_proc(true, false, nil).call(sample_request, sample_response)
       expect(@output).to eq expected_output
-    end
+    end 
 
     it 'should give "no response received" message for nil response' do
-      subject.set_http_trace_proc(true, false, nil).call(sample_request, nil)
+      subject.set_http_trace_proc(true, false, nil).call(nil, nil)
       expect(@output).to eq nil_response_output
     end
 
@@ -127,13 +142,32 @@ RSpec.describe Metasploit::Framework::LoginScanner::HTTP do
       expect(@output).to eq headers_only_output
     end
 
+    it 'should log HTTP requests and responses with body when HttpTraceHeadersOnly is unset' do
+      subject.set_http_trace_proc(true, nil, nil).call(sample_request, sample_response)
+      expect(@output).to eq expected_output
+    end
+
     it 'should log HTTP requests and responses in the specified color' do
       subject.set_http_trace_proc(true, false, 'blu/grn').call(sample_request, sample_response)
       expect(@output).to eq http_trace_colors_output
     end
 
-    it 'returns nil when HttpTrace is unset' do
+    it 'should only log HTTP request in color when only one colour is specified' do
+      subject.set_http_trace_proc(true, false, 'yel/').call(sample_request, sample_response)
+      expect(@output).to eq http_trace_single_color_output
+    end
+
+    it 'should only log HTTP request in color when only one color is specified without "/"' do
+      subject.set_http_trace_proc(true, false, 'yel').call(sample_request, sample_response)
+      expect(@output).to eq http_trace_single_color_output
+    end
+
+    it 'returns nil when HttpTrace is set to false' do
       expect(subject.set_http_trace_proc(false, false, nil)).to be_nil
+    end
+
+    it 'returns nil when HttpTrace is unset' do
+      expect(subject.set_http_trace_proc(nil, false, nil)).to be_nil
     end
   end
 
