a14a761387b9edda8ee25f517f5dbaec5e2b7482
3V3RYONE <beleswarprasad@gmail.com>
2022-10-25 21:33:29 +0530

    test request and response logger separately

--- spec/lib/metasploit/framework/login_scanner/http_spec.rb
+++ spec/lib/metasploit/framework/login_scanner/http_spec.rb
@@ -49,6 +49,31 @@ RSpec.describe Metasploit::Framework::LoginScanner::HTTP do
       res
     end
 
+    let(:normal_request_output) do
+      [
+        '####################',
+        '# Request:',
+        '####################',
+        "%clr%bld%redGET / HTTP/1.1\r",
+        "Host: \r",
+        "User-Agent: Met\r",
+        "Content-Length: 49\r",
+        "\r",
+        'bufaction=verifyLogin&user=admin&password=turnkey%clr',
+      ]
+    end
+    
+    let(:normal_response_output) do
+      [
+        '####################',
+        '# Response:',
+        '####################',
+        "%clr%bld%bluHTTP/1.1 302 Found\r",
+        "\r",
+        'Location: https://www.google.com/?gws_rd=ssl%clr'
+      ]
+    end
+
     let(:normal_request_response_output) do
       [
         '####################',
@@ -103,6 +128,31 @@ RSpec.describe Metasploit::Framework::LoginScanner::HTTP do
         "%clr%bld%bluHTTP/1.1 302 Found\r",
         "\r",
         '%clr'
+      ]
+    end
+
+    let(:http_trace_colors_output_request) do
+      [
+        '####################',
+        '# Request:',
+        '####################',
+        "%clr%bld%bluGET / HTTP/1.1\r",
+        "Host: \r",
+        "User-Agent: Met\r",
+        "Content-Length: 49\r",
+        "\r",
+        'bufaction=verifyLogin&user=admin&password=turnkey%clr',
+      ]
+    end
+    
+    let(:http_trace_colors_output_response) do
+      [
+        '####################',
+        '# Response:',
+        '####################',
+        "%clr%bld%grnHTTP/1.1 302 Found\r",
+        "\r",
+        'Location: https://www.google.com/?gws_rd=ssl%clr'
       ]
     end
 
@@ -186,61 +236,96 @@ RSpec.describe Metasploit::Framework::LoginScanner::HTTP do
       ]
     end
 
-    it 'returns a proc object when HttpTrace is set to true' do
-      expect(subject.set_http_trace_proc(true, false, nil)).to be_kind_of(Proc)
+    it 'returns a proc object to log request when HttpTrace is set to true' do
+      expect(subject.set_http_trace_proc_request(true, false, nil)).to be_kind_of(Proc)
+    end
+
+    it 'returns a proc object to log response when HttpTrace is set to true' do
+      expect(subject.set_http_trace_proc_response(true, false, nil)).to be_kind_of(Proc)
+    end
+
+    it 'should output the provided request with headers when HttpTrace is set' do
+      subject.set_http_trace_proc_request(true, false, nil).call(sample_request)
+      expect(@output).to eq normal_request_output
+    end
+
+    it 'should output the provided response with headers when HttpTrace is set' do
+      subject.set_http_trace_proc_response(true, false, nil).call(sample_response)
+      expect(@output).to eq normal_response_output
     end
 
     it 'should output the provided request and response with headers when HttpTrace is set' do
-      subject.set_http_trace_proc(true, false, nil).call(sample_request, sample_response)
+      subject.set_http_trace_proc_request(true, false, nil).call(sample_request)
+      subject.set_http_trace_proc_response(true, false, nil).call(sample_response)
       expect(@output).to eq normal_request_response_output
     end
-
+    
     it 'should give "no response received" message for nil response' do
-      subject.set_http_trace_proc(true, false, nil).call(sample_request, nil)
+      subject.set_http_trace_proc_request(true, false, nil).call(sample_request)
+      subject.set_http_trace_proc_response(true, false, nil).call(nil)
       expect(@output).to eq nil_response_output
     end
 
     it 'should log HTTP headers only when HttpTraceHeadersOnly is set' do
-      subject.set_http_trace_proc(true, true, nil).call(sample_request, sample_response)
+      subject.set_http_trace_proc_request(true, true, nil).call(sample_request)
+      subject.set_http_trace_proc_response(true, true, nil).call(sample_response)
       expect(@output).to eq headers_only_output
     end
 
     it 'should log HTTP requests and responses with body when HttpTraceHeadersOnly is unset' do
-      subject.set_http_trace_proc(true, nil, nil).call(sample_request, sample_response)
+      subject.set_http_trace_proc_request(true, nil, nil).call(sample_request)
+      subject.set_http_trace_proc_response(true, nil, nil).call(sample_response)
       expect(@output).to eq normal_request_response_output
     end
 
-    it 'should log HTTP requests and responses in the respective colors specified' do
-      subject.set_http_trace_proc(true, false, 'blu/grn').call(sample_request, sample_response)
+    it 'should log HTTP requests in the respective colors specified' do
+      subject.set_http_trace_proc_request(true, false, 'blu/grn').call(sample_request)
+      expect(@output).to eq http_trace_colors_output_request
+    end
+
+     it 'should log HTTP responses in the respective colors specified' do
+      subject.set_http_trace_proc_response(true, false, 'blu/grn').call(sample_response)
+      expect(@output).to eq http_trace_colors_output_response
+    end
+     
+     it 'should log HTTP requests and responses in the respective colors specified' do
+      subject.set_http_trace_proc_request(true, false, 'blu/grn').call(sample_request)
+      subject.set_http_trace_proc_response(true, false, 'blu/grn').call(sample_response)
       expect(@output).to eq http_trace_colors_output
     end
 
     it 'should only log HTTP request in color when only one color is specified followed by a trailing "/"' do
-      subject.set_http_trace_proc(true, false, 'yel/').call(sample_request, sample_response)
+      subject.set_http_trace_proc_request(true, false, 'yel/').call(sample_request)
+      subject.set_http_trace_proc_response(true, false, 'yel/').call(sample_response)
       expect(@output).to eq http_trace_single_color_output
     end
 
     it 'should only log HTTP response in color when only one color is specified after a leading "/"' do
-      subject.set_http_trace_proc(true, false, '/yel').call(sample_request, sample_response)
+      subject.set_http_trace_proc_request(true, false, '/yel').call(sample_request)
+      subject.set_http_trace_proc_response(true, false, '/yel').call(sample_response)
       expect(@output).to eq http_trace_single_color_output_leading_slash
     end
 
     it 'should not log HTTP request and response in color when HttpTraceColors is set to "/"' do
-      subject.set_http_trace_proc(true, false, '/').call(sample_request, sample_response)
+      subject.set_http_trace_proc_request(true, false, '/').call(sample_request)
+      subject.set_http_trace_proc_response(true, false, '/').call(sample_response)
       expect(@output).to eq http_trace_no_color_output
     end
 
     it 'should only log HTTP request in color when only one color is specified without any "/"s' do
-      subject.set_http_trace_proc(true, false, 'yel').call(sample_request, sample_response)
+      subject.set_http_trace_proc_request(true, false, 'yel').call(sample_request)
+      subject.set_http_trace_proc_response(true, false, 'yel').call(sample_response)
       expect(@output).to eq http_trace_single_color_output
     end
 
     it 'returns nil when HttpTrace is set to false' do
-      expect(subject.set_http_trace_proc(false, false, nil)).to be_nil
+      expect(subject.set_http_trace_proc_request(false, false, nil)).to be_nil
+      expect(subject.set_http_trace_proc_response(false, false, nil)).to be_nil
     end
 
     it 'returns nil when HttpTrace is unset' do
-      expect(subject.set_http_trace_proc(nil, false, nil)).to be_nil
+      expect(subject.set_http_trace_proc_request(nil, false, nil)).to be_nil
+      expect(subject.set_http_trace_proc_response(nil, false, nil)).to be_nil
     end
   end
 end
